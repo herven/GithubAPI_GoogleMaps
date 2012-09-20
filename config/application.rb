14ccdddcfcd644b69d7bc4eb8e8d0659c8adfabc
Raphaellito <raphaellito@raphaellito-VirtualBox.(none)>
2012-09-20 23:54:52 +0200

    asset precompiled fixing

--- config/application.rb
+++ config/application.rb
@@ -55,7 +55,7 @@ module GithubAPIGooglemaps
 
     # Enable the asset pipeline
     config.assets.enabled = true
-    config.assets.compile = true
+    config.assets.precompile += %w( *.js *.css )
     config.assets.initialize_on_precompile=false
 
     # Version of your assets, change this if you want to expire all your assets
