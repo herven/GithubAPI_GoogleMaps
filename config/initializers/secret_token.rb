fb9dae290decfffe7c26322eba91db7283f3f082
raphael <raphael.nti@jeemeo.com>
2012-09-20 10:21:56 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GithubAPIGooglemaps::Application.config.secret_token = '5927570c70960b531e13691c2bfa6af2ea84ef0d4766c921b4bdbcfb7604080da95eb7592091166fa379f3957a4358bc5958ccece6b4e4523d557ec7acba620d'
